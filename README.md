eaddc86722842ddcdcefcdcb71705eb3668cb9fd
Simon Elsbrock <simon@iodev.org>
2015-03-05 23:12:20 +0100

    Modify entrypoint script for our use case

---
+++ README.md
@@ -0,0 +1,2 @@
+Based on the official [tutum-docker-mysql](https://github.com/tutumcloud/tutum-docker-mysql/)
+Docker image. We use the phusion/baseimage-docker instead.
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+set -ex
 
 VOLUME_HOME="/var/lib/mysql"
-CONF_FILE="/etc/mysql/conf.d/my.cnf"
 LOG="/var/log/mysql/error.log"
 
 # Set permission of config file
-chmod 644 ${CONF_FILE}
 chmod 644 /etc/mysql/conf.d/mysqld_charset.cnf
 
 StartMySQL ()
@@ -28,53 +27,40 @@ StartMySQL ()
 
 CreateMySQLUser()
 {
-	StartMySQL
-	if [ "$MYSQL_PASS" = "**Random**" ]; then
-	    unset MYSQL_PASS
-	fi
+    StartMySQL
 
-	PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
-	_word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
-	echo "=> Creating MySQL user ${MYSQL_USER} with ${_word} password"
+    PASS=${MYSQL_PASS:-$(pwgen -s 20 1)}
+    _word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
+    echo "=> Creating MySQL user ${MYSQL_USER} with ${_word} password"
 
-	mysql -uroot -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '$PASS'"
-	mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION"
+    mysql -uroot -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '$PASS'"
+    mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION"
 
 
-	echo "=> Done!"
+    echo "=> Done!"
 
-	echo "========================================================================"
-	echo "You can now connect to this MySQL Server using:"
-	echo ""
-	echo "    mysql -u$MYSQL_USER -p$PASS -h<host> -P<port>"
-	echo ""
-	echo "Please remember to change the above password as soon as possible!"
-	echo "MySQL user 'root' has no password but only allows local connections"
-	echo "========================================================================"
+    echo "========================================================================"
+    echo "You can now connect to this MySQL Server using:"
+    echo ""
+    echo "    mysql -u$MYSQL_USER -p$PASS -h<host> -P<port>"
+    echo ""
+    echo "Please remember to change the above password as soon as possible!"
+    echo "========================================================================"
 
-	mysqladmin -uroot shutdown
+    mysqladmin -uroot shutdown
 }
 
 ImportSql()
 {
-	StartMySQL
+    StartMySQL
 
-	for FILE in ${STARTUP_SQL}; do
-	   echo "=> Importing SQL file ${FILE}"
-	   mysql -uroot < "${FILE}"
-	done
+    for FILE in ${STARTUP_SQL}; do
+       echo "=> Importing SQL file ${FILE}"
+       mysql -uroot < "${FILE}"
+    done
 
-	mysqladmin -uroot shutdown
+    mysqladmin -uroot shutdown
 }
-
-# Main
-if [ ${REPLICATION_MASTER} == "**False**" ]; then
-    unset REPLICATION_MASTER
-fi
-
-if [ ${REPLICATION_SLAVE} == "**False**" ]; then
-    unset REPLICATION_SLAVE
-fi
 
 # Initialize empty data volume and create MySQL user
 if [[ ! -d $VOLUME_HOME/mysql ]]; then
@@ -91,61 +77,4 @@ else
     echo "=> Using an existing volume of MySQL"
 fi
 
-# Import Startup SQL
-if [ -n "${STARTUP_SQL}" ]; then
-    if [ ! -f /sql_imported ]; then
-        echo "=> Initializing DB with ${STARTUP_SQL}"
-        ImportSql
-        touch /sql_imported
-    fi
-fi
-
-
-# Set MySQL REPLICATION - MASTER
-if [ -n "${REPLICATION_MASTER}" ]; then 
-    echo "=> Configuring MySQL replication as master ..."
-    if [ ! -f /replication_configured ]; then
-        RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
-        echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
-        sed -i "s/^#server-id.*/server-id = ${RAND}/" ${CONF_FILE}
-        sed -i "s/^#log-bin.*/log-bin = mysql-bin/" ${CONF_FILE}
-        echo "=> Starting MySQL ..."
-        StartMySQL
-        echo "=> Creating a log user ${REPLICATION_USER}:${REPLICATION_PASS}"
-        mysql -uroot -e "CREATE USER '${REPLICATION_USER}'@'%' IDENTIFIED BY '${REPLICATION_PASS}'"
-        mysql -uroot -e "GRANT REPLICATION SLAVE ON *.* TO '${REPLICATION_USER}'@'%'"
-        echo "=> Done!"
-        mysqladmin -uroot shutdown
-        touch /replication_configured
-    else
-        echo "=> MySQL replication master already configured, skip"
-    fi
-fi
-
-# Set MySQL REPLICATION - SLAVE
-if [ -n "${REPLICATION_SLAVE}" ]; then 
-    echo "=> Configuring MySQL replication as slave ..."
-    if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ] && [ -n "${MYSQL_PORT_3306_TCP_PORT}" ]; then
-        if [ ! -f /replication_configured ]; then
-            RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
-            echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
-            sed -i "s/^#server-id.*/server-id = ${RAND}/" ${CONF_FILE}
-            sed -i "s/^#log-bin.*/log-bin = mysql-bin/" ${CONF_FILE}
-            echo "=> Starting MySQL ..."
-            StartMySQL
-            echo "=> Setting master connection info on slave"
-            mysql -uroot -e "CHANGE MASTER TO MASTER_HOST='${MYSQL_PORT_3306_TCP_ADDR}',MASTER_USER='${MYSQL_ENV_REPLICATION_USER}',MASTER_PASSWORD='${MYSQL_ENV_REPLICATION_PASS}',MASTER_PORT=${MYSQL_PORT_3306_TCP_PORT}, MASTER_CONNECT_RETRY=30"
-            echo "=> Done!"
-            mysqladmin -uroot shutdown
-            touch /replication_configured
-        else
-            echo "=> MySQL replicaiton slave already configured, skip"
-        fi
-    else 
-        echo "=> Cannot configure slave, please link it to another MySQL container with alias as 'mysql'"
-        exit 1
-    fi
-fi
-
-tail -F $LOG &
-exec mysqld_safe
+chmod +x /etc/service/mysql/run
